3394aba2d740d37a43a897cac8cde7a68a9024c8
Richard Li <richardli125@gmail.com>
2019-07-15 16:01:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+  let text = document.querySelector('#text')
+  text.textContent = 'This is really cool!'
+  console.log('Hello Im last')
+});
+
+console.log('Second console.log')
+
