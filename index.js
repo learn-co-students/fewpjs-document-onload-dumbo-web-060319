e7b6fbe26ec68f7df3ed556a7aa92c2a15982c01
Justin Hutsell <hutselljustin@gmail.com>
2019-07-15 16:14:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+let text = document.querySelector('#text');
+
+document.addEventListener("DOMContentLoaded", function() {
+    text.textContent = "This is really cool!";
+  });
+   
